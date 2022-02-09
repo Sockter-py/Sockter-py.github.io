b81baea2a5de78aed82cb970af9c3f26e60f7981
Sockter-py <99278350+Sockter-py@users.noreply.github.com>
2022-02-09 11:53:16 -0500

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-import WebSocket;
-var Socket = new WebSocket('ws://' + window.location.hostname + ':999/');
-socket.onopen= function() {
-    var cmd = window. prompt(">>> "); 
-    socket.send(cmd);
-};
-socket.onmessage= function(s) {
-    alert('got reply '+s);
-};
