d1ca362dfd8c207b659230c0271bba0abc60af53
Sockter-py <99278350+Sockter-py@users.noreply.github.com>
2022-02-10 08:26:35 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
-var Server = require('ws').Server;
-var port = process.env.PORT || 9030;
-var ws = new Server({port: port});
-console.log(ws)
-console.log(port)
-console.log(Server)
-ws.on('connection', function(w){
-  
-  w.on('message', function(msg){
-    alert('message from client');
-  });
-  
-  w.on('close', function() {
-    alert('closing connection');
-  });
+let socket = new WebSocket("ws://localhost:1234/");
 
-});
+socket.onopen = function(e) {
+  alert("[open] Connection established");
+  alert("Sending to server");
+  socket.send("My name is John");
+};
+
+socket.onmessage = function(event) {
+  alert(`[message] Data received from server: ${event.data}`);
+};
+
+socket.onclose = function(event) {
+  if (event.wasClean) {
+    alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+  } else {
+    // e.g. server process killed or network down
+    // event.code is usually 1006 in this case
+    alert('[close] Connection died');
+  }
+};
+
+socket.onerror = function(error) {
+  alert(`[error] ${error.message}`);
+};
