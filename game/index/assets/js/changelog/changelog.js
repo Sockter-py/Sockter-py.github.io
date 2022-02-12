c0ea8a8775e26c398aec5b68718030716cdd05e9
Sockter-py <99278350+Sockter-py@users.noreply.github.com>
2022-02-12 15:06:33 -0500

    Add files via upload

---
+++ game/index/assets/js/changelog/changelog.js
@@ -0,0 +1,138 @@
+const logArr = [
+  {
+    version: `1.1.1`,
+    date: `2022-02-03 `,
+    list: [
+      {
+        label: [`New`],
+        detail: `Street Fighter 2`
+      },
+      {
+        label: [`New`],
+        detail: `Basketball Stars`
+      },
+      {
+        label: [`New`],
+        detail: `Wordle Unlimited`
+      },
+    ]
+  },
+  {
+    version: `1.1.0`,
+    date: `2022-02-01 `,
+    list: [
+      {
+        label: [`New`],
+        detail: `Spelunky`
+      },
+      {
+        label: [`New`],
+        detail: `Friday Night Funkin`
+      },
+      {
+        label: [`New`],
+        detail: `Smash Remix`
+      },
+      {
+        label: [`New`],
+        detail: `Age of War 2`
+      },
+      {
+        label: [`New`],
+        detail: `Monkeytype Lite`
+      },
+      {
+        label: [`New`],
+        detail: `Super Smash Flash`
+      },
+    ]
+  },
+  {
+    version: `1.0.1`,
+    date: `2022-01-30 `,
+    list: [
+      {
+        label: [`New`],
+        detail: `Pokemon Crystal, Alien Hominid, Raft Wars 1/2`
+      },
+      {
+        label: [`Fixed`],
+        detail: `Fixed the fullscreen bug with emulator games.`
+      },
+    ]
+  },
+  {
+    version: `1.0.0`,
+    date: `2022-01-29 `,
+    list: [
+      {
+        label: [`New`],
+        detail: `Changelog page to find all the updates, like this one.`
+      },
+      {
+        label: [`New`],
+        detail: `Renamed to "Games" instead of "Games List". URL also changed.`
+      },
+      {
+        label: [`New`],
+        detail: `Settings page added. You can find the tab cloaker there.`
+      },
+      {
+        label: [`New`],
+        detail: `Games page has been completely redone. New format + recommended games.`
+      },
+      {
+        label: [`New`],
+        detail: `Added Terms of Service + DMCA + Privacy Policy pages.`
+      },
+      {
+        label: [`New`],
+        detail: `Navbar changed to be easier to navigate.`
+      },
+      {
+        label: [`New`],
+        detail: `New footer to assist with navigation.`
+      },
+      {
+        label: [`Improved`],
+        detail: `Revamped home page and added some new elements.`
+      },
+      {
+        label: [`Improved`],
+        detail: `Compatability with proxies.`
+      },
+      {
+        label: [`Improved`],
+        detail: `Improved the FAQ page, made it look nicer + refactored some things.`
+      },
+      {
+        label: [`Removed`],
+        detail: `Lots of unnecessary assets/code.`
+      }
+    ]
+  },
+  /** Template for New Entries
+  {
+    version: `v1.0.0`,
+    date: `2022-01-01`,
+    list: [
+      {
+        label: [`New`],
+        detail: `New stuff`
+      },
+      {
+        label: [`Improved`],
+        detail: `Improved stuff`
+      },
+      {
+        label: [`Fixed`],
+        detail: `Fixed stuff`
+      },
+      {
+        label: [`Removed`],
+        detail: `Removed stuff`
+      }
+    ]
+  },
+  **/
+];
