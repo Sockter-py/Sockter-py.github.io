6afed61601d4a502283b637216a5fc2391f838c4
Sockter-py <99278350+Sockter-py@users.noreply.github.com>
2022-02-12 15:55:04 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,29 @@
 let socket = new WebSocket("ws://192.168.1.13:1234/");
 
-socket.onopen = function(e) {
-  alert("[open] Connection established");
-  alert("Sending to server");
-  let cmd = window.prompt("Enter your command: ");
-  socket.send(cmd);
-};
+while (true){
+    socket.onopen = function(e) {
+      alert("[open] Connection established");
+      alert("Sending to server");
+      let cmd = window.prompt("Enter your command: ");
+      socket.send(cmd);
+    };
 
-socket.onmessage = function(event) {
-  alert(`[message] Data received from server: ${event.data}`);
-};
+    socket.onmessage = function(event) {
+      alert(`[message] Data received from server: ${event.data}`);
+    };
 
- socket.onclose = function(event) {
-  if (event.wasClean) {
-    alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
-  } else {
-    // e.g. server process killed or network down
-    // event.code is usually 1006 in this case
-    alert('[close] Connection died');
-  }
-};
+     socket.onclose = function(event) {
+      if (event.wasClean) {
+        alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+      } else {
+        // e.g. server process killed or network down
+        // event.code is usually 1006 in this case
+        alert('[close] Connection died');
+      }
+    };
 
-socket.onerror = function(error) {
-  alert(`[error] ${error.message}`);
-};
+    socket.onerror = function(error) {
+      alert(`[error] ${error.message}`);
+    };
+
+}
