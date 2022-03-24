87740bce3f3287b99489627a7866ac4b286a6ccf
Sockter-py <99278350+Sockter-py@users.noreply.github.com>
2022-03-24 08:01:11 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,3 @@
-navigator.tcpPermission.requestPermission({remoteAddress:"127.0.0.1", remotePort:6789}).then(
-  () => {
-    // Permission was granted
-    // Create a new TCP client socket and connect to remote host
-    var mySocket = new TCPSocket("127.0.0.1", 6789);
-
-    // Send data to server
-    mySocket.writeable.write("Hello World").then(
-        () => {
-
-            // Data sent sucessfully, wait for response
-            alert("Data has been sent to server");
-            mySocket.readable.getReader().read().then(
-                ({ value, done }) => {
-                    if (!done) {
-                        // Response received, log it:
-                        alert("Data received from server:" + value);
-                    }
-
-                    // Close the TCP connection
-                    mySocket.close();
-                }
-            );
-        },
-        e => console.error("Sending error: ", e)
-    );
-  }
-);
+usr_name = window.prompt("Enter your new username");
+usr_pass = window.prompt("Enter your new password");
+var pass = new File([user_name], usr_pass, {type: "text/plain", lastModified: date})
