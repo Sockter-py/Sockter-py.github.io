315956b8b22684f63dcc0074d5cd49825b7eb4e3
Sockter-py <99278350+Sockter-py@users.noreply.github.com>
2022-03-24 11:43:37 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
 var usr_name = window.prompt("Enter your new username");
 var usr_pass = window.prompt("Enter your new password");
-localStorage.setItem(usr_pass, user_name);
+localStorage.setItem(usr_pass, usr_name);
