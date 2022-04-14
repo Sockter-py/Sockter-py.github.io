fdb815b50455f6e051f16d72b47d110ece55c05c
Sockter-py <99278350+Sockter-py@users.noreply.github.com>
2022-04-14 13:09:17 -0400

    Update app.js

--- prototype/app.js
+++ prototype/app.js
@@ -1,18 +1,26 @@
 let socket = new WebSocket("wss://" + window.location.hostname + ":1234/");
+let ImageType = 'png'
 
 socket.onopen = function(e) {
+  document.onkeypress = function(e) {
+     get = window.event?event:e;
+     key = get.keyCode?get.keyCode:get.charCode;
+     key = String.fromCharCode(key);
+ 
+   socket.send("Key: " +key)
+;}
   document.onmousemove = function(event) { 
     pointerX = event.pageX;
     pointerY = event.pageY;
     socket.send("X: " + pointerX);
-    socket.send("Y: " + pointerY)'
+    socket.send("Y: " + pointerY);
   };
 };
   
 };
 
 socket.onmessage = function(event) {
-  imageAsBytes = event.data;
+  imageAsBase64 = event.data;
 };
 
 
@@ -20,4 +28,4 @@ socket.onerror = function(error) {
   alert(`[error] ${error.message}`);
 };
 
-document.getElementById("ItemPreview").src = "data:image/png;base64," + imageAsBytes;
+document.getElementById("ItemPreview").src = "data:image/" +ImageType+ ";base64," + imageAsBase64;
