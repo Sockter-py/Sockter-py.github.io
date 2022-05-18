db796ed5665f583a15d2c673c84f3929403dc29a
Sockter-py <99278350+Sockter-py@users.noreply.github.com>
2022-05-18 11:32:20 -0400

    Update index.js

--- console/index.js
+++ console/index.js
@@ -1,5 +1,5 @@
 const console = document.getElementById('console');
-let input = document.getElementById('console').value;
+let input = console.value;
 
 if input == "cls"{
   input = ''
@@ -8,6 +8,16 @@ if input == "cls"{
 if (input.indexOf('ssh') !== -1) {
   pass = input.substring(input.indexOf("-p") +3);
   host = input.substring(input.indexOf("-h") + 3, input.indexOf("-p") -1);
+  try {
+    let socket = new WebSocket("ws://" + host + ":1234/");
+    socket.send(pass);
+    socket.onmessage = function(event) {
+      console.value = '\n' + event.data + ''  
+    }
+
+};
+  }
+    
 }
 console.value += '\n'+input+' is not a command';
 
