eec0566e18a8f2589a77bed1b13a21c926e7ff36
Sockter-py <99278350+Sockter-py@users.noreply.github.com>
2022-05-18 11:23:37 -0400

    Update index.js

--- console/index.js
+++ console/index.js
@@ -1,7 +1,14 @@
 const console = document.getElementById('console');
 let input = document.getElementById('console').value;
 
+if input == "cls"{
+  input = ''
+}
 
+if (input.indexOf('ssh') !== -1) {
+  pass = input.substring(input.indexOf("-p") +3);
+  host = input.substring(input.indexOf("-h") + 3, input.indexOf("-p") -1);
+}
 console.value += '\n'+input+' is not a command';
 
 
