e88bb9f52d0f7d8db61d9cd7753dbc8f8f06a21f
Sockter-py <99278350+Sockter-py@users.noreply.github.com>
2022-05-19 11:56:37 -0400

    Update index.js

--- console/index.js
+++ console/index.js
@@ -1,5 +1,5 @@
 
-
+let console = document.getElementById('console');
 document.addEventListener("keyup", function(event) {
     if (event.keyCode === 13) {
       let console = document.getElementById('console');
@@ -11,21 +11,22 @@ document.addEventListener("keyup", function(event) {
 
 function main(){
   if (input == "cls"){
-  input = ''
-}
-
-if (input.indexOf('ssh') !== -1) {
-  pass = input.substring(input.indexOf("-p") +3);
-  host = input.substring(input.indexOf("-h") + 3, input.indexOf("-p") -1);
-  try {
-    let socket = new WebSocket("ws://" + host + "/");
-    socket.send(pass);
-    socket.onmessage = function(event) {
-      console.value = '\n' + event.data + ''  
-    }
-  finally {
-      console.value += '\n An error has accored when \n connecting to victim. Try checking syntax:\n SSH -h [HOST:PORT] -p [PASSWORD]'
+    input = ''
   }
+
+  if (input.indexOf('ssh') !== -1) {
+      pass = input.substring(input.indexOf("-p") +3);
+      host = input.substring(input.indexOf("-h") + 3, input.indexOf("-p") -1);
+      try {
+        let socket = new WebSocket("ws://" + host + "/");
+        socket.send(pass);
+        socket.onmessage = function(event) {
+        console.value = '\n' + event.data + ''  
+      }
+
+      finally {
+          out = console + '\n' + 'An error has accored when \n connecting to victim. Try checking syntax:\n SSH -h [HOST:PORT] -p [PASSWORD]'
+      }
 
 
 
